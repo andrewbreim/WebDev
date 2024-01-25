da2bb94255cb91c1bcec9a96a7b9a17428ce7443
andrewbreim <228355@shakopeeschools.org>
2024-01-25 08:35:15 -0600

    things
    did some java

---
+++ Version7.0/user02/JS/script.js
@@ -0,0 +1,28 @@
+var space = " ";
+var pos = 0;
+var msg = "User 02";
+
+function Scroll() {
+    document.title = msg.substring(pos, msg.length) + space + msg.substring(0, pos);
+
+    pos++;
+    if (pos > msg.length) pos = 0;
+    window.setTimeout("Scroll()", 0);
+}
+Scroll();
+var space = " ";
+var pos = 0;
+var msg = "User 19";
+
+function Scroll() {
+    document.title = msg.substring(pos, msg.length) + space + msg.substring(0, pos);
+
+    pos++;
+    if (pos > msg.length) pos = 0;
+    window.setTimeout("Scroll()", 0);
+}
+Scroll();
+
+function changeText() {
+    document.getElementById("textChange").innerHTML = "Thanks for liking my Webpage";
+}
